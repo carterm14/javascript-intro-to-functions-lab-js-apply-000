6fd0b3af564b90df3a2796a5d05d8f7f124970a9
Michael Carter <mtcarter010@gmail.com>
2019-05-15 22:49:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const shout = string => {
   return string.toUpperCase();
 }
+
+const whisper = string => {
+  return string.toLowerCase();
+}
+
+const shout = string => {
+  return console.log(string.toUpperCase());
+}
+
+const shout = string => {
+  return string.toUpperCase();
+}
+
+const shout = string => {
+  return string.toUpperCase();
+}
+
+const shout = string => {
+  return string.toUpperCase();
+}
+
+const shout = string => {
+  return string.toUpperCase();
+}
